f2e6eabe4c4e8b7c644d9bc54da8c5705fd17722
Alexander <alexzund3r@yahoo.com>
2017-06-19 15:18:20 +0700

    Need to figure out how and where to check game result

--- js/game-presenter.js
+++ js/game-presenter.js
@@ -2,8 +2,11 @@ import renderScreen from './render-screen.js';
 import submitAnswer from './submit-answer.js';
 import guessGenreScreen from './containers/guess-genre.js';
 import guessArtistScreen from './containers/guess-artist.js';
+import resultsScreen from './containers/results.js';
 import genreQuestion from './model/genre-question.js';
 import artistQuestion from './model/artist-question.js';
+import victory from './model/victory.js';
+import defeat from './model/defeat.js';
 
 export default class GamePresenter {
   constructor(state, view) {
@@ -24,6 +27,15 @@ export default class GamePresenter {
 
   handleAnswerSubmit(answer) {
     this.state = submitAnswer(this.state, answer);
+
+    if (this.state.screen === `results`) {
+      if (this.state.lives === 0 || this.state.timeLeft === 0) {
+        renderScreen(resultsScreen(defeat));
+      } else {
+        renderScreen(resultsScreen(victory));
+      }
+      return;
+    }
 
     const randomNum = Math.random();
     if (randomNum > 0.5) {
