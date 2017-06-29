7dddbaea73545d2d8e57200996a013178ed1e0f3
Alexander <alexzund3r@yahoo.com>
2017-06-29 17:17:29 +0700

    Play btn position and timer fixes

--- js/views/guess-genre-view.js
+++ js/views/guess-genre-view.js
@@ -21,13 +21,9 @@ export default class GuessGenreView extends GameView {
           cx="390" cy="390" r="370"
           class="timer-line"
           style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
-
-        <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-          <span class="timer-value-mins">02</span><!--
-          --><span class="timer-value-dots">:</span><!--
-          --><span class="timer-value-secs">00</span>
-        </div>
       </svg>
+      <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
+      </div>
       <div class="main-wrap">
         <h2 class="title">${question}</h2>
         <form class="genre">
